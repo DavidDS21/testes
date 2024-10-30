796930162a674b9b88d4f28d701352d6f1fd3e50
DavidDS21 <david.santiago4@aluno.ce.gov.br>
2024-10-30 12:55:53 -0300

    ad  arquivos

---
+++ teste1/script.js
@@ -0,0 +1,58 @@
+let txt = 'delete';
+let vog = 0;
+for (i = 0; i < txt.length; i++) {
+    if (txt[i] == 'a' || txt[i] == 'e' || txt[i] == 'i' || txt[i] == 'o' || txt[i] == 'u') {
+        vog++;
+        console.log(txt[i] + ' -É uma vogal!');
+    }else {
+        console.log(txt[i] + ' -Não é uma vogal! ');
+    }    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //let vog = 0;
+    //for (i = 0; i < txt.length; i++){
+    //    if (txt[i] == 'a') {
+    //        vog =+ 1;
+    //    } else if (txt[i] == 'e') {
+    //        vog =+ 1;
+    //    } else if (txt[i] == 'i') {
+    //        vog =+ 1;
+    //    } else if (txt[i] == 'o') {
+    //        vog =+ 1;
+    //    } else if (txt[i] == 'u') {
+    //        vog =+ 1;
+    //    } else {
+    //        vog = 0;
+    //    }
+    //    console.log(vog)
+
