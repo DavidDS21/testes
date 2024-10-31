e8f01c836e65a6f3cc69dfb8104a536f329f2083
DavidDS21 <david.santiago4@aluno.ce.gov.br>
2024-10-31 09:10:13 -0300

    teste2
    testado

--- teste1/script.js
+++ teste1/script.js
@@ -1,18 +1,51 @@
-let txt = 'delete';
-let vog = 0;
-for (i = 0; i < txt.length; i++) {
-    if (txt[i] == 'a' || txt[i] == 'e' || txt[i] == 'i' || txt[i] == 'o' || txt[i] == 'u') {
-        vog++;
-        console.log(txt[i] + ' -É uma vogal!');
-    }else {
-        console.log(txt[i] + ' -Não é uma vogal! ');
-    }    
+// let txt = 'delete';
+// let vog = 0;
+// for (i = 0; i < txt.length; i++) {
+//     if (txt[i] == 'a' || txt[i] == 'e' || txt[i] == 'i' || txt[i] == 'o' || txt[i] == 'u') {
+//         vog++;
+//         console.log(txt[i] + ' -É uma vogal!');
+//     }else {
+//         console.log(txt[i] + ' -Não é uma vogal! ');
+//     }    
+//     }
+
+
+
+
+// function vogal() {
+//     let txt = document.getElementById('day').value;
+//     let vog = 0;
+//     for (i = 0; i < txt.length; i++) {
+//         if (txt[i] == 'a' || txt[i] == 'e' || txt[i] == 'i' || txt[i] == 'o' || txt[i] == 'u') {
+//             vog++;
+//             console.log(txt[i] + ' -É uma vogal!');
+//         }else {
+//             console.log(txt[i] + ' -Não é uma vogal! ');
+//         }    
+//     }
+//     document.getElementById('night').innerHTML = txt + vog;
+// }
+
+
+function verifica() {
+    let txt = document.getElementById('day').value;
+    let vog = 0;
+    for (i = 0; i < txt.length; i++) {
+        if (
+            txt[i] == 'a' ||
+            txt[i] == 'e' ||
+            txt[i] == 'i' ||
+            txt[i] == 'o' ||
+            txt[i] == 'u'
+    ) {
+            vog++;
+            console.log(txt[i] + ' -É uma vogal!');
+    }    else {
+            console.log(txt[i] + ' -Não é uma vogal! ');
     }
-
-
-
-
-
+    document.getElementById('night').innerHTML = 'A sua palavra tem ' + vog + " vogais" ;
+}
+}
 
 
 
