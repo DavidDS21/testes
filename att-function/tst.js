f643477efcc00f22bcb459a3e9e4cdc2901abe61
DavidDS21 <david.santiago4@aluno.ce.gov.br>
2024-11-14 09:11:33 -0300

    att-casa
    att-casa

---
+++ att-function/tst.js
@@ -0,0 +1,29 @@
+// QUESTÃO-01
+// function dobro(a, b, c){
+//     return 2 * a;
+    
+    
+// }
+// console.log(dobro(1123));
+
+// QUESTÃO-02
+// let x = prompt('Digite um número:')
+
+// function parImpar(){
+//     if (x % 2 == 0){
+//         alert( x + ' é um número Par!')
+//     } else {
+//         alert( x + " é um número ímpar!")
+//     }
+
+// } 
+// alert(parImpar(x))
+
+// let a = prompt('Digite um número:');
+// let b = prompt('Digite um número:');
+// let c = prompt('Digite um número:');
+
+// function calcMedia(a, b, c){
+//     return (a + b + c) / 3;
+// }
+// alert(calcMedia());
